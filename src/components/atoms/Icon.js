02cdb4c93343b0e74f88a4b93ff3a818059fe5ab
Soph H.P <smkendall90@gmail.com>
2022-02-02 18:03:57 +0100

    fixed Icon component

--- src/components/atoms/Icon.js
+++ src/components/atoms/Icon.js
@@ -2,9 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 const IconWrapper = styled.div`
-  width: 30px;
-  height: 30px;
-  padding: 1rem;
+  width: 46px;
+  height: 46px;
+  padding: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 100%;
+  }
 `;
 
 const Icon = ({ iconSource, alt }) => {
