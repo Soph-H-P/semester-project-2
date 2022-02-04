474e2cd554a977d9c63cae6e42170494536c0661
Soph H.P <smkendall90@gmail.com>
2022-02-04 07:58:24 +0100

    addded outline for content editor page and route

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Product from './pages/Product';
 import Products from './pages/Products';
 import Login from './pages/Login';
 import Bag from './pages/Bag';
+import ContentEditor from './pages/ContentEditor';
 import Favourites from './pages/Favourites';
 import Navigation from './components/organisms/Navagation';
 import Footer from './components/atoms/Footer';
@@ -28,6 +29,7 @@ const App = () => {
             <Route path="/login" element={<Login />}></Route>
             <Route path="/favourites" element={<Favourites />}></Route>
             <Route path="/your-bag" element={<Bag />}></Route>
+            <Route path="/content-editor" element={<ContentEditor />}></Route>
           </Routes>
         </div>
         <Footer>&copy; Tracks 2022 </Footer>
